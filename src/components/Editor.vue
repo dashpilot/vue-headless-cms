01b46db295888f5755b1ebadf35a2757f047060b
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-05 23:58:44 +0100

    Added link function to the rich text editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,11 @@
 <template>
 <div class="btn-group mt-1 w-100">
-  <button class="btn btn-outline-secondary" @click="editor.chain().focus().toggleBold().run()">Bold</button>
-  <button class="btn btn-outline-secondary" @click="editor.chain().focus().toggleItalic().run()">Italic</button>
+  <button class="btn btn-outline-secondary" @click="editor.chain().focus().toggleBold().run()"><i class="fas fa-bold"></i></button>
+  <button class="btn btn-outline-secondary" @click="editor.chain().focus().toggleItalic().run()"><i class="fas fa-italic"></i></button>
+
+  <button @click="setLink" class="btn btn-outline-secondary">
+    <i class="fas fa-link"></i>
+  </button>
   <button class="btn btn btn-outline-secondary w-75" disabled></button>
 </div>
 
@@ -14,6 +18,7 @@ import {
   EditorContent
 } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+import Link from '@tiptap/extension-link'
 
 export default {
   components: {
@@ -32,6 +37,41 @@ export default {
       editor: null,
     }
   },
+
+  methods: {
+    setLink() {
+      const previousUrl = this.editor.getAttributes('link').href
+      const url = window.prompt('URL', previousUrl)
+
+      // cancelled
+      if (url === null) {
+        return
+      }
+
+      // empty
+      if (url === '') {
+        this.editor
+          .chain()
+          .focus()
+          .extendMarkRange('link')
+          .unsetLink()
+          .run()
+
+        return
+      }
+
+      // update link
+      this.editor
+        .chain()
+        .focus()
+        .extendMarkRange('link')
+        .setLink({
+          href: url
+        })
+        .run()
+    },
+  },
+
 
   watch: {
     modelValue(value) {
@@ -53,6 +93,9 @@ export default {
     this.editor = new Editor({
       extensions: [
         StarterKit,
+        Link.configure({
+          openOnClick: false,
+        }),
       ],
       content: this.modelValue,
       onUpdate: () => {
