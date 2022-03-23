5448e0e6ff7f29a20fb5548db7da9c60ec6bf673
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-23 13:13:49 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
 import {
-  createApp
+  createApp,
+  provide
 } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const myPlugin = {
+  install(app, options) {
+    // configure the app
+    console.log('plugin loaded')
+    const logout = () => {
+      console.log("Logout is being called");
+    };
+
+    app.provide("logout", logout);
+
+    async function postData(url = '', data = {}) {
+
+      const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    app.provide("postData", postData);
+
+  }
+}
+
+app.use(myPlugin, {})
+app.mount('#app')
--- src/widgets/Gallery.vue
+++ src/widgets/Gallery.vue
@@ -1,5 +1,15 @@
+<script setup>
+import {
+  inject
+} from 'vue'
+const logout = inject('logout')
+const postData = inject('postData')
+</script>
+
 <template>
 <div>
+
+  <button @click="logout">Log out</button>
 
   <input type="file" :id="'fileInput-'+rand" accept="image/*" @change="uploadImage" style="display: none;">
 
@@ -202,22 +212,6 @@ export default {
     },
   }
 
-}
-
-async function postData(url = '', data = {}) {
-
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
 }
 </script>
 
