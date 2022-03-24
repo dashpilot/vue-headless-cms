3699855538f1117efecb5f70c13fb70e6ecb5e47
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-24 12:22:26 +0100

    Moved the array_move method to the global scope

--- src/components/SortableList.vue
+++ src/components/SortableList.vue
@@ -144,16 +144,6 @@ export default {
       }
 
     },
-    array_move(arr, old_index, new_index) {
-      if (new_index >= arr.length) {
-        var k = new_index - arr.length + 1;
-        while (k--) {
-          arr.push(undefined);
-        }
-      }
-      arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-      return arr;
-    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -8,13 +8,14 @@ const app = createApp(App)
 // example plugin
 const myPlugin = {
   install(app, options) {
-    // configure the app
-    console.log('plugin loaded')
 
+    /*
+    console.log('plugin loaded')
     const logout = () => {
       console.log("Logout is being called");
     };
     app.provide("logout", logout);
+    */
 
     app.mixin({
       methods: {
@@ -27,6 +28,16 @@ const myPlugin = {
             .replace(/-+$/, ''); // Trim - from end of text
           console.log(slug)
           return slug;
+        },
+        array_move(arr, old_index, new_index) {
+          if (new_index >= arr.length) {
+            var k = new_index - arr.length + 1;
+            while (k--) {
+              arr.push(undefined);
+            }
+          }
+          arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+          return arr;
         },
         closeWindow() {
           this.$emit('update:show', false);
